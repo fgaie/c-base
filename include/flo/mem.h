f424f479633a84092be1d3665fa1f72acad2fb09
fgaie <63815040+fgaie@users.noreply.github.com>
2024-05-19 14:49:28 +0000

    Update release to output generated at 88c260c

---
+++ include/flo/mem.h
@@ -0,0 +1,20 @@
+#ifndef BASE_MEM_H
+#define BASE_MEM_H
+
+#include <flo.h>
+
+typedef struct Arena Arena;
+
+Arena *arena_new(void);
+void arena_free(Arena *);
+
+void *arena_alloc(Arena *, u64 size);
+void *arena_allocz(Arena *, u64 size);
+
+void *arena_realloc(Arena *, void *p, u64 size);
+
+void arena_pop(Arena *, u64 size);
+
+void arena_reset(Arena *);
+
+#endif // BASE_MEM_H
